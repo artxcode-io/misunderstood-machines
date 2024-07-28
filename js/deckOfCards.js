15b41c0b0c6779cda1ff73bd71042ef8a01967fa
Sofia Garcia <sofisagarcia@gmail.com>
2024-07-28 11:56:54 -0400

    add

---
+++ js/deckOfCards.js
@@ -0,0 +1,634 @@
+// lolololol get outttta here 
+
+const deckOfCards = {
+    "personalityDeck": [
+        {
+            "id": 1,
+            "rank": "2",
+            "suit": "hearts",
+            "text": "calm"
+        },
+        {
+            "id": 2,
+            "rank": "3",
+            "suit": "hearts",
+            "text": "soft"
+        },
+        {
+            "id": 3,
+            "rank": "4",
+            "suit": "hearts",
+            "text": "delicate"
+        },
+        {
+            "id": 4,
+            "rank": "5",
+            "suit": "hearts",
+            "text": "misunderstood"
+        },
+        {
+            "id": 5,
+            "rank": "6",
+            "suit": "hearts",
+            "text": "sensitive"
+        },
+        {
+            "id": 6,
+            "rank": "7",
+            "suit": "hearts",
+            "text": "compassionate"
+        },
+        {
+            "id": 7,
+            "rank": "8",
+            "suit": "hearts",
+            "text": "gentle"
+        },
+        {
+            "id": 8,
+            "rank": "9",
+            "suit": "hearts",
+            "text": "graceful"
+        },
+        {
+            "id": 9,
+            "rank": "10",
+            "suit": "hearts",
+            "text": "tender"
+        },
+        {
+            "id": 10,
+            "rank": "J",
+            "suit": "hearts",
+            "text": "delicate"
+        },
+        {
+            "id": 11,
+            "rank": "Q",
+            "suit": "hearts",
+            "text": "vulnerable"
+        },
+        {
+            "id": 12,
+            "rank": "K",
+            "suit": "hearts",
+            "text": "stoic"
+        },
+        {
+            "id": 13,
+            "rank": "A",
+            "suit": "hearts",
+            "text": "nurturing"
+        },
+        {
+            "id": 14,
+            "rank": "2",
+            "suit": "diamonds",
+            "text": "peaceful"
+        },
+        {
+            "id": 15,
+            "rank": "3",
+            "suit": "diamonds",
+            "text": "eccentric"
+        },
+        {
+            "id": 16,
+            "rank": "4",
+            "suit": "diamonds",
+            "text": "perceptive"
+        },
+        {
+            "id": 17,
+            "rank": "5",
+            "suit": "diamonds",
+            "text": "sincere"
+        },
+        {
+            "id": 18,
+            "rank": "6",
+            "suit": "diamonds",
+            "text": "charming"
+        },
+        {
+            "id": 19,
+            "rank": "7",
+            "suit": "diamonds",
+            "text": "peculiar"
+        },
+        {
+            "id": 20,
+            "rank": "8",
+            "suit": "diamonds",
+            "text": "sly"
+        },
+        {
+            "id": 21,
+            "rank": "9",
+            "suit": "diamonds",
+            "text": "scheming"
+        },
+        {
+            "id": 22,
+            "rank": "10",
+            "suit": "diamonds",
+            "text": "grotesque"
+        },
+        {
+            "id": 23,
+            "rank": "J",
+            "suit": "diamonds",
+            "text": "cranky"
+        },
+        {
+            "id": 24,
+            "rank": "Q",
+            "suit": "diamonds",
+            "text": "strange"
+        },
+        {
+            "id": 25,
+            "rank": "K",
+            "suit": "diamonds",
+            "text": "whimsical"
+        },
+        {
+            "id": 26,
+            "rank": "A",
+            "suit": "diamonds",
+            "text": "mysterious"
+        },
+        {
+            "id": 27,
+            "rank": "2",
+            "suit": "clubs",
+            "text": "toxic"
+        },
+        {
+            "id": 28,
+            "rank": "3",
+            "suit": "clubs",
+            "text": "mystical"
+        },
+        {
+            "id": 29,
+            "rank": "4",
+            "suit": "clubs",
+            "text": "neurotic"
+        },
+        {
+            "id": 30,
+            "rank": "5",
+            "suit": "clubs",
+            "text": "arrogant"
+        },
+        {
+            "id": 31,
+            "rank": "6",
+            "suit": "clubs",
+            "text": "impassioned"
+        },
+        {
+            "id": 32,
+            "rank": "7",
+            "suit": "clubs",
+            "text": "defiant"
+        },
+        {
+            "id": 33,
+            "rank": "8",
+            "suit": "clubs",
+            "text": "condescending"
+        },
+        {
+            "id": 34,
+            "rank": "9",
+            "suit": "clubs",
+            "text": "moody"
+        },
+        {
+            "id": 35,
+            "rank": "10",
+            "suit": "clubs",
+            "text": "violent"
+        },
+        {
+            "id": 36,
+            "rank": "J",
+            "suit": "clubs",
+            "text": "timid"
+        },
+        {
+            "id": 37,
+            "rank": "Q",
+            "suit": "clubs",
+            "text": "confrontational"
+        },
+        {
+            "id": 38,
+            "rank": "K",
+            "suit": "clubs",
+            "text": "devious"
+        },
+        {
+            "id": 39,
+            "rank": "A",
+            "suit": "clubs",
+            "text": "flirty"
+        },
+        {
+            "id": 40,
+            "rank": "2",
+            "suit": "spades",
+            "text": "camp"
+        },
+        {
+            "id": 41,
+            "rank": "3",
+            "suit": "spades",
+            "text": "esoteric"
+        },
+        {
+            "id": 42,
+            "rank": "4",
+            "suit": "spades",
+            "text": "rude"
+        },
+        {
+            "id": 43,
+            "rank": "5",
+            "suit": "spades",
+            "text": "pretentious"
+        },
+        {
+            "id": 44,
+            "rank": "6",
+            "suit": "spades",
+            "text": "angelic"
+        },
+        {
+            "id": 45,
+            "rank": "7",
+            "suit": "spades",
+            "text": "facetious"
+        },
+        {
+            "id": 46,
+            "rank": "8",
+            "suit": "spades",
+            "text": "philosophical"
+        },
+        {
+            "id": 47,
+            "rank": "9",
+            "suit": "spades",
+            "text": "terrorizing"
+        },
+        {
+            "id": 48,
+            "rank": "10",
+            "suit": "spades",
+            "text": "bratty"
+        },
+        {
+            "id": 49,
+            "rank": "J",
+            "suit": "spades",
+            "text": "dainty"
+        },
+        {
+            "id": 50,
+            "rank": "Q",
+            "suit": "spades",
+            "text": "fickle"
+        },
+        {
+            "id": 51,
+            "rank": "K",
+            "suit": "spades",
+            "text": "patronizing"
+        },
+        {
+            "id": 52,
+            "rank": "A",
+            "suit": "spades",
+            "text": "inconsiderate"
+        }
+    ],
+   "rigidDeck": [
+            {
+                "id": 1,
+                "rank": "2",
+                "suit": "hearts",
+                "text": "machines"
+            },
+            {
+                "id": 2,
+                "rank": "3",
+                "suit": "hearts",
+                "text": "algorithms"
+            },
+            {
+                "id": 3,
+                "rank": "4",
+                "suit": "hearts",
+                "text": "neural networks"
+            },
+            {
+                "id": 4,
+                "rank": "5",
+                "suit": "hearts",
+                "text": "functions"
+            },
+            {
+                "id": 5,
+                "rank": "6",
+                "suit": "hearts",
+                "text": "conditionals"
+            },
+            {
+                "id": 6,
+                "rank": "7",
+                "suit": "hearts",
+                "text": "nodes"
+            },
+            {
+                "id": 7,
+                "rank": "8",
+                "suit": "hearts",
+                "text": "arrays"
+            },
+            {
+                "id": 8,
+                "rank": "9",
+                "suit": "hearts",
+                "text": "variables"
+            },
+            {
+                "id": 9,
+                "rank": "10",
+                "suit": "hearts",
+                "text": "databases"
+            },
+            {
+                "id": 10,
+                "rank": "J",
+                "suit": "hearts",
+                "text": "frameworks"
+            },
+            {
+                "id": 11,
+                "rank": "Q",
+                "suit": "hearts",
+                "text": "states"
+            },
+            {
+                "id": 12,
+                "rank": "K",
+                "suit": "hearts",
+                "text": "recursion"
+            },
+            {
+                "id": 13,
+                "rank": "A",
+                "suit": "hearts",
+                "text": "pointers"
+            },
+            {
+                "id": 14,
+                "rank": "2",
+                "suit": "diamonds",
+                "text": "methods"
+            },
+            {
+                "id": 15,
+                "rank": "3",
+                "suit": "diamonds",
+                "text": "objects"
+            },
+            {
+                "id": 16,
+                "rank": "4",
+                "suit": "diamonds",
+                "text": "integers"
+            },
+            {
+                "id": 17,
+                "rank": "5",
+                "suit": "diamonds",
+                "text": "screen readers"
+            },
+            {
+                "id": 18,
+                "rank": "6",
+                "suit": "diamonds",
+                "text": "terminals"
+            },
+            {
+                "id": 19,
+                "rank": "7",
+                "suit": "diamonds",
+                "text": "compilers"
+            },
+            {
+                "id": 20,
+                "rank": "8",
+                "suit": "diamonds",
+                "text": "parameters"
+            },
+            {
+                "id": 21,
+                "rank": "9",
+                "suit": "diamonds",
+                "text": "multisigs"
+            },
+            {
+                "id": 22,
+                "rank": "10",
+                "suit": "diamonds",
+                "text": "binary trees"
+            },
+            {
+                "id": 23,
+                "rank": "J",
+                "suit": "diamonds",
+                "text": "linters"
+            },
+            {
+                "id": 24,
+                "rank": "Q",
+                "suit": "diamonds",
+                "text": "for loops"
+            },
+            {
+                "id": 25,
+                "rank": "K",
+                "suit": "diamonds",
+                "text": "captcha"
+            },
+            {
+                "id": 26,
+                "rank": "A",
+                "suit": "diamonds",
+                "text": "packets"
+            },
+            {
+                "id": 27,
+                "rank": "2",
+                "suit": "clubs",
+                "text": "divs"
+            },
+            {
+                "id": 28,
+                "rank": "3",
+                "suit": "clubs",
+                "text": "floats"
+            },
+            {
+                "id": 29,
+                "rank": "4",
+                "suit": "clubs",
+                "text": "consoles"
+            },
+            {
+                "id": 30,
+                "rank": "5",
+                "suit": "clubs",
+                "text": "APIs"
+            },
+            {
+                "id": 31,
+                "rank": "6",
+                "suit": "clubs",
+                "text": "browsers"
+            },
+            {
+                "id": 32,
+                "rank": "7",
+                "suit": "clubs",
+                "text": "DNS records"
+            },
+            {
+                "id": 33,
+                "rank": "8",
+                "suit": "clubs",
+                "text": "servers"
+            },
+            {
+                "id": 34,
+                "rank": "9",
+                "suit": "clubs",
+                "text": "breakpoints"
+            },
+            {
+                "id": 35,
+                "rank": "10",
+                "suit": "clubs",
+                "text": "bugs"
+            },
+            {
+                "id": 36,
+                "rank": "J",
+                "suit": "clubs",
+                "text": "caching"
+            },
+            {
+                "id": 37,
+                "rank": "Q",
+                "suit": "clubs",
+                "text": "CSS"
+            },
+            {
+                "id": 38,
+                "rank": "K",
+                "suit": "clubs",
+                "text": "JavaScript"
+            },
+            {
+                "id": 39,
+                "rank": "A",
+                "suit": "clubs",
+                "text": "HTML"
+            },
+            {
+                "id": 40,
+                "rank": "2",
+                "suit": "spades",
+                "text": "redirects"
+            },
+            {
+                "id": 41,
+                "rank": "3",
+                "suit": "spades",
+                "text": "<span> tags"
+            },
+            {
+                "id": 42,
+                "rank": "4",
+                "suit": "spades",
+                "text": "git merges"
+            },
+            {
+                "id": 43,
+                "rank": "5",
+                "suit": "spades",
+                "text": "abstraction"
+            },
+            {
+                "id": 44,
+                "rank": "6",
+                "suit": "spades",
+                "text": "terminal commands"
+            },
+            {
+                "id": 45,
+                "rank": "7",
+                "suit": "spades",
+                "text": "text editors"
+            },
+            {
+                "id": 46,
+                "rank": "8",
+                "suit": "spades",
+                "text": "booleans"
+            },
+            {
+                "id": 47,
+                "rank": "9",
+                "suit": "spades",
+                "text": "callbacks"
+            },
+            {
+                "id": 48,
+                "rank": "10",
+                "suit": "spades",
+                "text": "private keys"
+            },
+            {
+                "id": 49,
+                "rank": "J",
+                "suit": "spades",
+                "text": "data structures"
+            },
+            {
+                "id": 50,
+                "rank": "Q",
+                "suit": "spades",
+                "text": "integrations"
+            },
+            {
+                "id": 51,
+                "rank": "K",
+                "suit": "spades",
+                "text": "kernels"
+            },
+            {
+                "id": 52,
+                "rank": "A",
+                "suit": "spades",
+                "text": "run-time errors"
+            }
+        ]
+}
+
+// And yet, here you are. It's ok. m u a h
---
+++ js/sketch.js
@@ -0,0 +1,197 @@
+/***********************************************
+ * 
+ * Sneaky, sneaky. 
+ * 
+ ************************************************/ 
+
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+
+  return deck;
+}
+
+const suitColors = {
+  spades: "#1A1919",
+  clubs: "#1A1919",
+  hearts: "#F24822",
+  diamonds: "#F24822",
+};
+
+function getColorForsuit(suit) {
+  return suitColors[suit] || "#F24822";
+}
+
+function createSvgPath(attributes) {
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  Object.keys(attributes).forEach((key) => {
+    path.setAttribute(key, attributes[key]);
+  });
+  return path;
+}
+
+function getIconForsuit(suit) {
+  const svgAttributes = {
+    spades: { width: "21", height: "24", viewBox: "0 0 21 24" },
+    hearts: { width: "20", height: "23", viewBox: "0 0 20 23" },
+    diamonds: { width: "20", height: "21", viewBox: "0 0 20 21" },
+    clubs: { width: "20", height: "22", viewBox: "0 0 20 22" },
+  };
+
+  const paths = {
+    spades: {
+      "fill-rule": "evenodd",
+      "clip-rule": "evenodd",
+      d: "M15.1987 18.4534C19.0674 18.5033 21.4473 15.6566 20.1382 12.6601C18.8292 9.66359 17.5241 8.98941 15.4692 6.81696C13.4133 4.64451 10.4238 0.886434 10.4379 9.8632e-08C10.453 0.886434 7.58663 4.64451 5.53069 6.81696C3.47577 8.98941 2.17176 9.66359 0.861691 12.6601C-0.447365 15.6566 1.93356 18.5033 5.80118 18.4534C8.45664 18.419 9.65164 16.667 10.1391 15.5716L10.1391 17.0052C10.1391 17.0052 9.29133 22.1765 3.30822 22.7426L2.35444 22.7426L2.35444 23.5484L18.6465 23.5484L18.6465 22.7426L17.6917 22.7426C11.7096 22.1765 10.8608 17.0052 10.8608 17.0052L10.8608 15.7598C11.4351 16.8564 12.6906 18.4209 15.1987 18.4534Z",
+      fill: "#1A1919",
+    },
+    hearts: {
+      "fill-rule": "evenodd",
+      "clip-rule": "evenodd",
+      d: "M14.6989 0.75236C10.8308 0.811816 10.0618 5.21556 10.0618 5.21556C10.0618 5.21556 9.16923 0.811816 5.30116 0.75236C1.43299 0.692809 -0.947437 4.08485 0.361833 7.65545C1.67101 11.226 2.97547 12.0294 5.03096 14.618C7.08637 17.2067 10.0767 21.6848 10.0618 22.7411C10.047 21.6848 12.9136 17.2067 14.9691 14.618C17.0246 12.0294 18.329 11.226 19.6382 7.65545C20.9474 4.08485 18.567 0.692809 14.6989 0.75236Z",
+      fill: "#F24822",
+    },
+    diamonds: {
+      "fill-rule": "evenodd",
+      "clip-rule": "evenodd",
+      d: "M0 10.3174L10 20.6349L20 10.3174L10 0L0 10.3174Z",
+      fill: "#F24822",
+    },
+    clubs: {
+      "fill-rule": "evenodd",
+      "clip-rule": "evenodd",
+      d: "M15.4219 7.42603C14.7988 7.42603 14.2051 7.56203 13.6637 7.80741C14.1721 7.00462 14.4704 6.03207 14.4704 4.98322C14.4704 2.23109 12.4206 0 9.89225 0C7.36385 0 5.31413 2.23109 5.31413 4.98322C5.31413 5.98181 5.58502 6.91096 6.04962 7.6906C5.58768 7.51997 5.09304 7.42603 4.57811 7.42603C2.04962 7.42603 0 9.65711 0 12.4092C0 15.1614 2.04962 17.3925 4.57811 17.3925C5.14914 17.3925 5.69535 17.2777 6.19947 17.0697C6.19938 17.0709 8.33008 16.4446 9.7307 13.6697V15.3405C9.7307 15.3405 9.01258 20.6071 3.94976 21.1836H3.14178V22.0043H16.93V21.1836H16.1221C11.0593 20.6071 10.3411 15.3405 10.3411 15.3405V13.7822C10.6019 14.4075 12.0443 17.4649 15.2859 17.389C15.3311 17.3902 15.3763 17.3925 15.4219 17.3925C17.9504 17.3925 20 15.1614 20 12.4092C20 9.65711 17.9504 7.42603 15.4219 7.42603Z",
+      fill: "#1A1919",
+    },
+  };
+
+  const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const path = createSvgPath(paths[suit]);
+
+  Object.entries(svgAttributes[suit]).forEach(([key, value]) => {
+    svgElement.setAttribute(key, value);
+  });
+  svgElement.appendChild(path);
+  return svgElement;
+}
+
+window.onload = function () {
+  const { personalityDeck, rigidDeck } = deckOfCards;
+
+  const adjective = document.querySelector("#adjective");
+  const noun = document.querySelector("#noun");
+  const rankAdjective = document.querySelectorAll(".rank-adjective");
+  const rankNoun = document.querySelectorAll(".rank-noun");
+  const suitAdjective = document.querySelectorAll("#suit-adjective");
+  const suitNoun = document.querySelectorAll("#suit-noun");
+
+  const updateCardContent = (element, deck, suitElement, rankElement) => {
+    const randomCard = shuffle(deck)[0];
+
+    element.innerHTML = randomCard.text;
+    suitElement.forEach((item) => item.replaceChildren(getIconForsuit(randomCard.suit)));
+    rankElement.forEach((item) => {
+      item.innerHTML = randomCard.rank;
+      item.style.color = getColorForsuit(randomCard.suit);
+    });
+  };
+
+  const shuffleAdjectives = () => updateCardContent(adjective, personalityDeck, suitAdjective, rankAdjective);
+  const shuffleNouns = () => updateCardContent(noun, rigidDeck, suitNoun, rankNoun);
+
+  shuffleAdjectives();
+  shuffleNouns();
+
+  const cards = document.querySelectorAll(".card");
+
+
+document.querySelector(".playing-cards").addEventListener("click", (event) => {
+  const card = event.target.closest(".card");
+  if (!card) return;
+
+  const flipCard = () => card.classList.toggle("flip");
+
+  const shuffleCard = () => {
+    if (!card.classList.contains("flip")) {
+      if (card.id.includes("sensitive")) {
+        setTimeout(shuffleAdjectives, 800);
+      }
+
+      if (card.id.includes("rigid")) {
+        setTimeout(shuffleNouns, 800);
+      }
+    }
+  };
+  flipCard();
+  shuffleCard();
+  
+ 
+});
+
+
+
+cards.forEach((card) =>
+  card.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      const card = event.target.closest(".card");
+      if (!card) return;
+
+      const flipCard = () => card.classList.toggle("flip");
+
+      flipCard();
+    }
+  })
+);
+};
+
+
+
+              
+/***********************************************
+ * 
+ * I appreciate that you looked,
+ * but I'm equally kinda horrified? 
+ *  
+ * I think we should make this a fair exchange, no?
+ * 
+ * Send me a book recommendation
+ * Send me a fun lil secret
+ * Send me a photo you've taken recently
+ * Send me an essay on.. anything?
+ * 
+ * Go crazy. Or don't. We're friends now.
+ *
+ * Subject: Misunderstood Machines
+ * sofia@artxcode.io
+ * 
+ *
+ ************************************************/ 
+
+
+
+/**
+   * 
+   * 
+   *           _
+      /b_,dM\__,_
+    _/MMMMMMMMMMMm,
+   _YMMMMMMMMMMMM(
+  `MMMMMM/   /   \   _   ,    
+   MMM|  __  / __/  ( |_|
+   YMM/_/# \__/# \    | |_)arry
+   (.   \__/  \__/     ___  
+     )       _,  |    '_|_)
+_____/\     _   /       | otter
+    \  `._____,'
+     `..___(__
+              ``-.
+                  \
+              gnv  )
+
+
+
+   HP was here */
+
+  
